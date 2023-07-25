b16a247d04cd60aaed63fecb32b91d49f412db6e
maxplayerone <antekmajner@gmail.com>
2023-07-25 12:31:31 +0200

    Colored triangle

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,41 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+use wgpu::util::DeviceExt;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex{
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex{
+    fn desc() -> wgpu::VertexBufferLayout<'static>{
+        wgpu::VertexBufferLayout{
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute{
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute{
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex{position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
+    Vertex{position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
+    Vertex{position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+];
 
 struct State {
     surface: wgpu::Surface,
@@ -12,6 +47,8 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_of_vertices: u32,
 }
 
 impl State {
@@ -97,7 +134,9 @@ impl State {
             vertex: wgpu::VertexState{
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[],
+                buffers: &[
+                    Vertex::desc(),
+                ],
             },
             fragment: Some(wgpu::FragmentState{
                 module: &shader,
@@ -126,6 +165,16 @@ impl State {
             multiview: None,
         });
 
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor{
+                label: Some("Vertex buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
+        let num_of_vertices = VERTICES.len() as u32;
+
         Self {
             window,
             surface,
@@ -134,6 +183,8 @@ impl State {
             config,
             size,
             render_pipeline,
+            vertex_buffer,
+            num_of_vertices,
         }
     }
 
@@ -190,7 +241,8 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_of_vertices, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
