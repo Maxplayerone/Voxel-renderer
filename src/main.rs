1f3d3fc45c652e1b26697d88c07a5fe2ce7275e7
maxplayerone <antekmajner@gmail.com>
2023-07-25 12:11:43 +0200

    Triangle

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,5 @@
-use glutin::event::{Event, WindowEvent};
-use glutin::event_loop::ControlFlow;
-
-mod renderer;
+use forsaken_west::run;
 
 fn main() {
-    unsafe {
-        let (gl, window, event_loop) = {
-            let event_loop = glutin::event_loop::EventLoop::new();
-            let window_builder = glutin::window::WindowBuilder::new()
-                .with_title("Hello triangle!")
-                .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-            let window = glutin::ContextBuilder::new()
-                .with_vsync(true)
-                .build_windowed(window_builder, &event_loop)
-                .unwrap()
-                .make_current()
-                .unwrap();
-            let gl =
-                glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
-            (gl, window, event_loop)
-        };
-
-        let mut renderer = renderer::Renderer::new(gl);
-
-        event_loop.run(move |event, _, control_flow| {
-            *control_flow = ControlFlow::Wait;
-            match event {
-                Event::LoopDestroyed => {
-                    return;
-                }
-                Event::MainEventsCleared => {
-                    window.window().request_redraw();
-                }
-                Event::RedrawRequested(_) => {
-                    renderer.render();
-                    window.swap_buffers().unwrap();
-                }
-                Event::WindowEvent { ref event, .. } => match event {
-                    WindowEvent::Resized(physical_size) => {
-                        window.resize(*physical_size);
-                    }
-                    WindowEvent::CloseRequested => {
-                        renderer.delete();
-                        *control_flow = ControlFlow::Exit
-                    }
-                    WindowEvent::KeyboardInput{device_id: _, input: _, is_synthetic: _} => {
-                    }
-                    _ => (),
-                },
-                _ => (),
-            }
-        });
-    }
+    pollster::block_on(run());
 }
