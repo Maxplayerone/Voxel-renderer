33ea54ab97f3d488f5012606a9ae0fae37263c89
maxplayerone <antekmajner@gmail.com>
2023-07-25 12:47:31 +0200

    index buffer

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,20 @@ impl Vertex{
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex{position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
-    Vertex{position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex{position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0]},
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
 ];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
+
 
 struct State {
     surface: wgpu::Surface,
@@ -48,7 +58,8 @@ struct State {
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_of_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_of_indices: u32,
 }
 
 impl State {
@@ -173,7 +184,15 @@ impl State {
             }
         );
 
-        let num_of_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(
+          &wgpu::util::BufferInitDescriptor{
+                label: Some("index buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }  
+        );
+
+        let num_of_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -184,7 +203,8 @@ impl State {
             size,
             render_pipeline,
             vertex_buffer,
-            num_of_vertices,
+            num_of_indices,
+            index_buffer,
         }
     }
 
@@ -242,7 +262,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_of_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_of_indices, 0, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
